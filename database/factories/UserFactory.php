d2c25412410361465bb71ea8eb77aeeb5651c745
R7elvis <rickyelvis.t@gmail.com>
2024-04-30 11:43:55 +0300

    Using Database seeders to add data to the db tables

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,7 +30,6 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
-            'admin' => false
         ];
     }
 
@@ -43,5 +42,4 @@ class UserFactory extends Factory
             'email_verified_at' => null,
         ]);
     }
-
 }
