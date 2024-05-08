3945996aff76c9c0110c932b6e4d37afed023e6a
R7elvis <rickyelvis.t@gmail.com>
2024-05-08 08:51:43 +0300

    Using queues to handle jobs and logmessages

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,18 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
 use App\Mail\JobPosted;
+use App\Models\Job;
 use Illuminate\Support\Facades\Auth;
 
+Route::get('test', function () {
 
+    $job = Job::first();
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
@@ -21,8 +29,8 @@ Route::get('/jobs/create', [JobController::class, 'create']);
 Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
 Route::get('/jobs/{job}', [JobController::class, 'show']);
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
-->middleware('auth')
-->can('edit', 'job');
+    ->middleware('auth')
+    ->can('edit', 'job');
 
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
